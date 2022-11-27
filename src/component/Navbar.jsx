fa2f64d182c136a50f69c3267b5dc53b5afec74e
gilangwokeh <gilangwokeh12@gmail.com>
2022-11-27 16:02:46 +0700

    update

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -5,15 +5,15 @@ import Navbar from 'react-bootstrap/Navbar';
 const NavbarHome = () => {
   return (
     <div>
-    <Navbar bg="primary" expand="lg">
+    <Navbar bg="info" expand="lg">
       <Container>
         <Navbar.Brand href="#home">
-          <img src="./indihome.png" alt="indihome" className='w-12 '/>
+          <img src="./indihome.png" alt="Indihome Tangerang" className='w-12 '/>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto sm:ml-[70%]">
-            <Nav.Link href="https://indihomejakartabarat.com/" className='text-black'>Indihome Jakarta Barat</Nav.Link>
+            <Nav.Link href="https://pasangindihomesolo.web.id/" className='text-black'>Indihome Tangerang</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,14 +7,14 @@ const Home = () => {
   return (
     <>
       <NavbarHome />
-      <h1 className='text-center font-bold text-2x1 text-gray-800 pt-5 text-[22px]'>Daftar Indihome Jakarta Barat Di Tahun 2023 Dan Rilis Terbaru</h1>
+      <h1 className='text-center font-bold text-2x1 text-gray-800 pt-5 text-[22px]'>Daftar Indihome Tangerang Di Tahun 2023 Dan Rilis Terbaru</h1>
       <div className='container mx-auto px-2 max-w-5xl pt-5 md:pt-32'>
-        <img className='w-[60%] mx-auto' src="./Indihome-jakarta-barat.jpg" alt="indihome jakarta barat" />
-        <p className='pt-3 pb-2 text-justify'>Suatu Berita Menguntungkan bagi Kamu  Dan Keluarga Kamu yang memerlukan pemasangan Indihome .Segera Daftar  <a className='text-blue-600 hover:text-red-600' href="https://indihomejakartabarat.com/">Indihome Jakarta Barat</a> Di  Tahun 2023 secara cepat dan mudah dengan menggunakan website pemasangan Indihome yang kami sediakan untuk kamu ingin memasang wifi indihome di rumah/kantor dan kamar apartemen kamu .</p>
+        <img className='w-[60%] mx-auto' src="./indihome-Tangerang.jpg" alt="Indihome Tangerang" />
+        <p className='pt-3 pb-2 text-justify'>Suatu Berita Menguntungkan bagi Kamu  Dan Keluarga Kamu yang memerlukan pemasangan Indihome .Segera Daftar  <a className='text-blue-600 hover:text-red-600' href="https://pasangindihomesolo.web.id/">Indihome Tangerang</a> Di  Tahun 2023 secara cepat dan mudah dengan menggunakan website pemasangan Indihome yang kami sediakan untuk kamu ingin memasang wifi indihome di rumah/kantor dan kamar apartemen kamu .</p>
         <p className='pt-2 pb-2 text-justify'>Silahkan hidupkan handphone mu lalu  hubungi sales/marketing resmi Indihome Fiber (Speedy) Telkom Jakarta Barat melalui website ini untuk pasang WIFI Indihome di rumah/kantor dan kamar apartemen Kamu.</p>
         <p className='pt-2 pb-2 text-justify'>Indihome menawarkan koneksi internet unlimited (tanpa batasan kuota) dengan teknologi fiber optic. Saat ini, tersedia ada  layanan Triple Play dan Dual Play yang bisa Anda pilih. Triple Play terdiri dari Internet Fiber, Telepon Rumah, dan TV Interaktif (Usee TV). Adapun Dual Play terdiri dari Internet Fiber dan Telepon Rumah atau Internet Fiber dan TV Interaktif (Usee TV).</p>
-        <h2 className='text-center font-bold pt-2'>Harga Paket Indihome Jakarta Barat Tahun 2023</h2>
-        <p className='pt-2 pb-2 text-justify'>Semua harga paket indihome Jakarta Barat 2023 yang kami tampilkan merupakan produk terbaik dan terbaru</p>
+        <h2 className='text-center font-bold pt-2'>Harga Paket Indihome Tangerang Tahun 2023</h2>
+        <p className='pt-2 pb-2 text-justify'>Semua harga paket Indihome Tangerang 2023 yang kami tampilkan merupakan produk terbaik dan terbaru</p>
         <p className='pt-2 pb-2 text-justify'>Silahkan Kawan, Kerabat, Saudara semuanya silahkan call , sms , WA, tidak usah datang dan antri di plasa TELKOM cukup kontak kami dan kami akan datang ke tempat anda. Kami akan informasikan Paket-paket lain yang lebih terjangkau dan HARGA PROMO untuk wilayah tertentu.</p>
         <Paket />
         <h3 className='text-center font-bold p-4'>Paket 3P (PAKET TRIPLE PLAY)</h3>
@@ -61,8 +61,7 @@ const Home = () => {
           <li className='ml-5'>Speed 100 Mbps: Rp 795.000 (belum Termasuk PPn 11%)</li>
         </ul>
         <a className='text-blue-600 hover:text-red-600' href="https://wa.me/6285348092993?text=Halo%20kak%20Saya%20ingin%20pasang%20wifi%20Indihome%20Paket%202P%20Kak?">Klik Untuk Order Pemasangan Wifi Indihome Paket 2P Internet Dan Telpon Lewat WhatsApp Saya</a>
-        <h3 className='font-bold text-center p-4'>Biaya Tarif Pasang Baru Indihome Jakarta Barat Tahun 2023</h3>
-        <img className='w-[60%] mx-auto' src='./pasang-indihome-Jakarta-barat.jpg' alt="indihome jakarta barat" />
+        <h3 className='font-bold text-center p-4'>Biaya Tarif Pasang Baru Indihome Tangerang Tahun 2023</h3>
         <p className='pt-2 pb-2'> secara harga biaya atau tarif pemasangan baru Indihome di Jakarta Barat adalah sebagai berikut:</p>
         <ul className='list-disc ml-5 pb-2'>
           <li>Biaya pemasangan (cukup sekali saja/tidak lebih dari sekali)</li>
