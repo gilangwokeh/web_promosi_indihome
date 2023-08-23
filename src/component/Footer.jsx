1a1b38404f58fa6082f04ac8939d17b7a268a60b
gilangwokeh <gilangwokeh12@gmail.com>
2023-08-23 17:55:36 +0700

    update

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='bg-black mt-4 h-9'>
-      <h1 className='text-[70%] text-center pt-[10px] text-white md:text-[100%] md:text-center md:pt-2'>Hak Cipta © 2022 Indihome Tangerang All rights reserved. </h1>
+      <h1 className='text-[70%] text-center pt-[10px] text-white md:text-[100%] md:text-center md:pt-2'>Hak Cipta © 2023 Indihome Tangerang All rights reserved. </h1>
     </div>
   )
 }
